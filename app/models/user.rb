d4221040c90e4c306b843a4f22d3356cc4798619
Josh McMillan <josh@joshmcmillan.co.uk>
2014-01-22 22:10:49 +0000

    Some work on the initial skeleton

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         omniauth_providers: [:facebook]
+
+  def self.find_for_facebook_oauth(auth)
+    where(provider: auth.provider, provider_uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider     = auth.provider
+      user.provider_uid = auth.uid
+      user.email        = auth.info.email
+      user.password     = Devise.friendly_token[0,20]
+      user.first_name   = auth.info.first_name
+      user.last_name    = auth.info.last_name
+      user.save!
+    end
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+end
